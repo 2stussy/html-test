23b83b63a8693b0ab6d50a7a0998f9d63689c5da
stas <kovalzhi.stas@mail.ru>
2025-11-10 09:59:31 +0300

    minimal update

--- src/script.js
+++ src/script.js
@@ -4,3 +4,9 @@ function hello(argument){
 function goodbye(argument){
     console.log('bye')
 }
+function hello(argument){
+    console.log('hello')
+}
+function goodbye(argument){
+    console.log('bye')
+}
